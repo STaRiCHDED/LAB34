ce078ffe843b86b8976b911d90b00b1ebf865572
STaRiCHDED <nik179804@gmail.com>
2021-01-06 22:34:06 +0300

    lab042

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,7 +1,7 @@
 // Copyright 2020 Nikita Klimov nik179804@gmail.com
 #include "Analysis.hpp"
 
-int main(int argc, char* argv[]) {
+int main() {
   std::string path;
   Analysis A("/home/starich/STARICHDED/SEM3/LAB34-REW/misc//ftp");
   A.printData("/home/starich/STARICHDED/SEM3/LAB34-REW/misc//ftp");
